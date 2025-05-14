0df5b1dc2f89a4dec058122d7eb7788350be3f36
dnzcsgn <dnzcsgndc@gmail.com>
2025-05-14 16:59:42 -0700

    updated

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+// Step 1: Variable Data
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Step 2: Function and Block
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// Step 3: Variable Declaration and Scope
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+
+  function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+  }
+
+ 
+  global.changeFeaturedDrink = changeFeaturedDrink;
+}
